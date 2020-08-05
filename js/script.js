3fce370b31a84bde6e798d356c457a175643275f
enmozz <43096220+enmozz@users.noreply.github.com>
2020-08-05 12:34:48 +0700

    css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.hamburger').on("click", function() {
+        $(".nav-grid").toggleClass("open");
+    });
+});
+    
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
